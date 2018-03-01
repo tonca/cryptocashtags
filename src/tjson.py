53f90e705b11e64bf3b2cfe49ebd0ecd4d359f71
tonca <istsamtonca@gmail.com>
2018-03-01 17:08:15 +0100

    Some more comments

--- src/tjson.py
+++ src/tjson.py
@@ -1,31 +1,33 @@
 import json
 
+# Number of followers
 def n_flwrs(data):
     return json.loads(data)['user']['followers_count']
 
+# Number of friends (following?)
 def n_frnds(data):
     return json.loads(data)['user']['friends_count']
 
+# Is the status a retweet
 def is_retweet(data):
     return 'retweeted_status' in json.loads(data)
 
-# Return the number of retweets of a retweet
+# Number of retweets of a retweeted status
 def n_retweets(data):
     if is_retweet(data):
         return int(json.loads(data)['retweeted_status']['retweet_count'])
     else:
         return 0
 
-def tweet_symbols(data):
-    info = json.loads(data)
-    return info['entities']['symbols']
-
+# Does the status contain links?
 def has_link(data):
     return len(json.loads(data)['entities']['urls'])>0
 
+# Does the status contain links?
 def has_mntns(data):
     return len(json.loads(data)['entities']['user_mentions'])>0
 
+# The status text
 def tweet_text(data):
     info = json.loads(data)
     if 'extended_tweet' in info:
@@ -34,6 +36,12 @@ def tweet_text(data):
         text = info['text']
     return text
 
+# Are there more than one cashtag in the status
 def is_multiple_cashtag(tweet):
     info = json.loads(tweet)
     return len(info['entities']['symbols'])>1
+
+# The JSON string describing the symbols in a status text ()
+def tweet_symbols(data):
+    info = json.loads(data)
+    return info['entities']['symbols']
